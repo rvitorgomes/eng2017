3cd51da3f315b6db1b63d69dd294164c7eef5656
Rubens Gomes <vitorr_gomes@hotmail.com>
2017-09-21 03:10:56 -0300

    fix path build

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('../dist'));
 
 const mountRoutes = require('./routes');
 mountRoutes(app);
